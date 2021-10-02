b7c9bdf4b409668d10cda56db613701158fe2a0a
Ronaldson Bellande <rbelland@dan417-32.cs.uml.edu>
2021-10-02 19:26:26 -0400

    for now

--- computer_vision_analysis.py
+++ computer_vision_analysis.py
@@ -13,6 +13,10 @@ if __name__ == "__main__":
         # Classify the images
         if sys.argv[1] == "model_training":
             computer_vision_analysis_obj = computer_vision_training(model_type = sys.argv[2], image_type = sys.argv[3], category = sys.argv[4])
+        
+        # Indentify and image with model
+        if sys.argv[1] == "image_classification_with_model":
+            pass
 
         # Localization and Detection 
         if sys.argv[1] == "localization_and_detection":
