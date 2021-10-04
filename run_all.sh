a4047fc3ea394ef0f273052cc42006c83fc88de5
Ronaldson Bellande <rbelland@dan417-32.cs.uml.edu>
2021-10-03 20:38:24 -0400

    finished for later

--- run_all.sh
+++ run_all.sh
@@ -1,14 +1,17 @@
  #python3 computer_vision_analysis.py model_training model1 small_traffic_sign normal
- python3 computer_vision_analysis.py model_training model1 train1 category_1
- python3 computer_vision_analysis.py model_training model1 train1 category_2
- python3 computer_vision_analysis.py model_training model1 train1 category_3
+ #python3 computer_vision_analysis.py model_training model1 train1 category_1
+ #python3 computer_vision_analysis.py model_training model1 train1 category_2
+ #python3 computer_vision_analysis.py model_training model1 train1 category_3
  #python3 computer_vision_analysis.py model_training model1 train1 regular
+ python3 computer_vision_analysis.py model_training model1 train2 regular
 
  #python3 computer_vision_analysis.py model_training model2 small_traffic_sign normal
- python3 computer_vision_analysis.py model_training model2 train1 category_1
- python3 computer_vision_analysis.py model_training model2 train1 category_2
- python3 computer_vision_analysis.py model_training model2 train1 category_3
+ #python3 computer_vision_analysis.py model_training model2 train1 category_1
+ #python3 computer_vision_analysis.py model_training model2 train1 category_2
+ #python3 computer_vision_analysis.py model_training model2 train1 category_3
  #python3 computer_vision_analysis.py model_training model2 train1 regular
+ python3 computer_vision_analysis.py model_training model2 train2 regular
+
 
  #python3 computer_vision_analysis.py model_training model3 normal
 
